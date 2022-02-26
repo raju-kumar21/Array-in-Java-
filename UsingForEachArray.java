77d84e19e171a848e976f9b0ead160118379d233
RAJU KUMAR <97028776+raju-kumar21@users.noreply.github.com>
2022-02-26 14:51:47 +0530

    Create UsingForEachArray.java

---
+++ UsingForEachArray.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a[]={1,2,3,4,5};
+	    
+	    for(int i:a)
+	    {
+	       System.out.println("array element:"+i); 
+	    }
+	    
+		
+	}
+}
