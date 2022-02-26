469e7a7e8d16a0d215701c07914afcf04bf9fec6
RAJU KUMAR <97028776+raju-kumar21@users.noreply.github.com>
2022-02-26 14:48:07 +0530

    Create MainArray.java

---
+++ MainArray.java
@@ -0,0 +1,14 @@
+public class MainArray
+{
+	public static void main(String[] args) {
+    //declaration, instantiation and initialization  
+	    int a[]={1,2,3,4,5};
+	    
+	    for(int i=0;i<a.length;i++)
+	    {
+	       System.out.println("array element:"+a[i]); 
+	    }
+	    
+		
+	}
+}
